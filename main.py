cc803337cf9096312335f1c05855de8f47044265
Sergey <mazi77@bk.ru>
2022-07-17 15:51:01 +0500

    Добавлена функция парсинга курса валют

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from parse_page import *
 
 if __name__ == '__main__':
     page = get_page('16.07.2022')
-    print(parse_page(page))
-
+    for c in parse_page(page):
+        print(c)
+        
--- parse_page.py
+++ parse_page.py
@@ -1,6 +1,13 @@
 from bs4 import BeautifulSoup
 
 def parse_page(page):
-    soup = BeautifulSoup(page)
-    courses = soup.find('table', {'class': 'data'})
+    soup = BeautifulSoup(page, 'html.parser')
+    courses = []
+    table = soup.find('table', {'class': 'data'}).find('tbody').children
+    for row in table:
+        if str(row) != '\n':
+            soup = BeautifulSoup(str(row), 'html.parser')
+            data = soup.find('tr').findAll('td')
+            data = [str(entry).replace('<td>', '').replace('</td>', '') for entry in data]
+            courses.append(data)
     return courses
